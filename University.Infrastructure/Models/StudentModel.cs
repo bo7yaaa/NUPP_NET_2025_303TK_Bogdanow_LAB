6d60559ec2aac816b8f4e61c2e1727d56fc28ecf
bo7yaaa <kripaxexe@gmail.com>
2025-05-19 19:14:37 +0300

    Додано Лабораторну роботу 3

---
+++ University.Infrastructure/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Infrastructure.Models;
+
+public class StudentModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public int GroupId { get; set; }
+    public GroupModel Group { get; set; }
+}
